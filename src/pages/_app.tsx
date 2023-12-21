981d1247729ba4702410d6a2de068748cf05981a
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-12-21 16:07:24 +0900

    ログイン機能追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { initializeFirebaseApp } from '@/lib/firebase/firebase'
+import { getApp } from 'firebase/app'
 
+initializeFirebaseApp()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log(getApp())
+
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
